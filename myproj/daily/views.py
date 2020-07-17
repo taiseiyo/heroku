b633acf5047d69ee5c151b72783c24e95360a692
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-07-17 19:45:08 +0900

    fix

--- myproj/daily/views.py
+++ myproj/daily/views.py
@@ -17,17 +17,17 @@ def form(request):
     if(request.method == "POST"):
 
         if(request.POST["name"] == "taiseiyo" and request.POST["password"] == "FGxG9wei"):
-            template = loader.get_template('daily/index.html')
+            template = loader.get_template('daily/taisei/index.html')
             return HttpResponse(template.render(None, request))
 
-    return render(request, "daily/form.html", params)
+    return render(request, "daily/taisei/form.html", params)
 
 
 def index(request):
-    template = loader.get_template('daily/index.html')
+    template = loader.get_template('daily/taisei/index.html')
     return HttpResponse(template.render(None, request))
 
 
 def taiseiyo(request):  # 新しくnew関数を追記
-    template_name = "daily/new.html"
+    template_name = "daily/taisei/new.html"
     return render(request, template_name)
