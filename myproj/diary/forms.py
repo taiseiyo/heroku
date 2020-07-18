928f7fc17347882ca78cd283838a2921e1525308
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-07-18 17:48:43 +0900

    fix

--- myproj/diary/forms.py
+++ myproj/diary/forms.py
@@ -2,6 +2,10 @@ from django import forms
 
 
 class DailyForm(forms.Form):
+
+    usr_data = ["one", "taisei"]
+    choice = forms.MultipleChoiceField(label="usr", choices=usr_data,
+                                       widget=forms.SelectMultiple(attrs={"size": 1}))
     name = forms.CharField(label="name",
                            widget=forms.TextInput(
                                attrs={"class": "form-control"}))
