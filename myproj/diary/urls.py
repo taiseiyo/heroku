a2f6a96dd20dc44abd8daff75523b6e501274902
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-07-18 15:11:42 +0900

    fix

--- myproj/diary/urls.py
+++ myproj/diary/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # name は views.py 以下の関数に合わせる
     # name と 第2引数は合わせる
     # path("", views.index, name="index"),
-    path("", DiaryForm.form(), name="form"),
-    path("taiseiyo/", DiaryForm.taiseiyo(), name="taiseiyo"),  # 追記
+    path("", DiaryForm.form, name="form"),
+    path("taiseiyo/", DiaryForm.taiseiyo, name="taiseiyo"),  # 追記
 ]
