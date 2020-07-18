7912c271ff2d6fa1dcba0f019cbf51adb94d6521
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-07-18 18:28:29 +0900

    fix

--- myproj/diary/forms.py
+++ myproj/diary/forms.py
@@ -3,13 +3,14 @@ from django import forms
 
 class DailyForm(forms.Form):
 
-    usr_data = [("one", "taiseiyo")]
+    usr_data = [("one", "taiseiyo"),
+                ("two", "syasin")]
 
     choice = forms.ChoiceField(label="user name", choices=usr_data)
 
-    name = forms.CharField(label="name",
-                           widget=forms.TextInput(
-                               attrs={"class": "form-control"}))
+    # name = forms.CharField(label="name",
+    #                        widget=forms.TextInput(
+    #                            attrs={"class": "form-control"}))
 
     password = forms.CharField(label="password",
                                widget=forms.TextInput(
