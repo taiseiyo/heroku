d2ca1682af1beb1140bf1739b97d952a1be08a6d
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-07-18 15:13:51 +0900

    fix

--- myproj/diary/urls.py
+++ myproj/diary/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import DiaryForm
+from .views import DiaryView
 
 urlpatterns = [
     # name は views.py 以下の関数に合わせる
     # name と 第2引数は合わせる
     # path("", views.index, name="index"),
-    path("", DiaryForm.form, name="form"),
-    path("taiseiyo/", DiaryForm.taiseiyo, name="taiseiyo"),  # 追記
+    path("", DiaryView.form, name="form"),
+    path("taiseiyo/", DiaryView.taiseiyo, name="taiseiyo"),  # 追記
 ]
