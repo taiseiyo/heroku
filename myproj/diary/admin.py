7f3bbf01c81637f145d9e8a044b3c11f748577b7
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-07-21 18:57:46 +0900

    add

--- myproj/diary/admin.py
+++ myproj/diary/admin.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
 from django.contrib import admin
 from .models import ActiveUser
 
 # Register your models here.
 
-admin.site.register(AcutiveUser)
+admin.site.register(ActiveUser)
--- myproj/diary/models.py
+++ myproj/diary/models.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from django.db import models
 
 # Create your models here.
