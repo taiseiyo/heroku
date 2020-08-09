cc9f40a7b1fe23cba3f732139bdfdd3a3e6920c3
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-08-10 07:44:32 +0900

    fix

--- myproj/diary/static/sample.js
+++ myproj/diary/static/sample.js
@@ -1,16 +1,28 @@
-function scrollToTop(){
-    scrollTo(0,0);
+function scrollToTop() {
+  scrollTo(0, 0);
 }
 
-function scrollToDown(){
-    let h = document.documentElement.scrollHeight;
-    scrollTo(0,h);
+function scrollToDown() {
+  let h = document.documentElement.scrollHeight;
+  scrollTo(0, h);
 }
 
-// 即時関数
+function slide_picture() {
+  let pic = new Array(
+    "../../../static/star.jpg",
+    "../../../static/star2.jpg",
+    "../../../static/castle.jpg",
+  );
 
+  if (num == 2) {
+    num = 0;
+  } else {
+    num = num + 1;
+  }
+  document.getElementById("mypic").src = pic[num];
+  setTimeout("slide_picture()", 5000);
+}
 
+slide_picture();
 
-
-
-
+// 即時関数作ってみる
