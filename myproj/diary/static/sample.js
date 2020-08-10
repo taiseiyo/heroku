13708614722370f5ae30a6121209b4cdaef947d2
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-08-10 12:40:04 +0900

    fix

--- myproj/diary/static/sample.js
+++ myproj/diary/static/sample.js
@@ -22,7 +22,7 @@ function slide_picture() {
   document.getElementById("mypic").src = pic[num];
   // const img = document.querySelector("#mypic");
   const img = document.getElementById("mypic");
-  img.animate([{opacity: "0.2"}, {opacity: "1"}], 2000);
+  img.animate([{opacity: "0"}, {opacity: "1"}], 2000);
   setTimeout("slide_picture()", 5000);
 }
 slide_picture();
