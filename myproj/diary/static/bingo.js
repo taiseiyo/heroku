7d14c2cbb77ead0ee009e5143b2a265e5dfcd605
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-09-22 23:30:55 +0900

    fix

--- myproj/diary/static/bingo.js
+++ myproj/diary/static/bingo.js
@@ -1,5 +1,6 @@
 let arr = new Array(72);
 let serial_num = 1; // 連番用
+let count = 0; //ループを止めるスイッチ用
 
 for (let i = 0; i < arr.length; i++) {
   arr[i] = serial_num;
@@ -17,3 +18,18 @@ while (a) {
 }
 
 let ids = ["id1", "id2", "id3", "id4", "id5", "id6", "id7", "id8", "id9"];
+
+function display_suzi() {
+  let min = 1;
+  let max = 72;
+  let a = Math.floor(Math.random() * (max + 1 - min)) + min;
+  document.getElementById("id10").textContent = a;
+}
+
+function startTimer() {
+  testTimer = setInterval("display_suzi()", 10);
+}
+
+function stopTimer() {
+  clearInterval(testTimer);
+}
