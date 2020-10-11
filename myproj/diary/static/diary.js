e605812d4bd87c1360f1b9d4966444c874ae52a1
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-10-11 15:41:49 +0900

    fix

--- myproj/diary/static/diary.js
+++ myproj/diary/static/diary.js
@@ -23,9 +23,27 @@ function slide_picture() {
   document.getElementById("mypic").src = pic[num];
   // const img = document.querySelector("#mypic");
   const img = document.getElementById("mypic");
-  img.animate([{opacity: "0.4"}, {opacity: "1"}], 4000);
+  img.animate([{opacity: "0.4"}, {opacity: "1"}], 10000);
 }
 
-setInterval(slide_picture, 4000);
+setInterval(slide_picture, 10000);
 
-// 即時関数作ってみる
+let value = 0;
+let imgWidth = -780;
+let flag = true;
+function animation_background() {
+  document.getElementById("skinBody").style.backgroundPosition = value + "px";
+  if (flag == true) {
+    value = value - 1;
+    if (value == imgWidth) {
+      flag = false;
+    }
+  } else if (flag == false) {
+    value = value + 1;
+    if (value == 0) {
+      flag = true;
+    }
+  }
+}
+
+setInterval(animation_background, 20);
